d756f319a6ae5a688ac18ca0c8186b9d3983ad85
Jaya-Swedhaa <jayaswedhaa@bitcot.com>
2024-07-05 17:26:08 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,14 @@
+def greet(name: str) -> str:
+    return f"Hello this is, {name}!"
+
+greeting_name = "Jaya"
+greeting = greet(greeting_name)
+print(greeting)
+
+
+
+def YOB(year: int)-> int:
+    return f"Year of birth is {year}"
+
+Year1 = YOB(2002)
+print(Year1)
